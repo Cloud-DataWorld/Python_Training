a53977d429c7000832bbcb3fdb5ffc29ba2620a1
Cloud-DataWorld <cloudsolutionsde@gmail.com>
2023-12-20 23:30:18 -0600

    class 1,2,3

---
+++ Pythonscripts/2_identifiers.py
@@ -0,0 +1,27 @@
+# An identifier is a name given to entities like class, functions, variables, etc
+# it differentiate one entity from another
+
+# 1. Identifier can't statt with a digit
+# 1var = 10
+# print(1var)
+
+# 2. Identifier can't use special symbols
+# val2@ = 35
+# print(val2@)
+
+# 3. Keywords can't be used as identifiers
+
+# import = 123
+# print(import)
+
+# 4. correct way of defining an identifier
+# Identifiers can be a combination of letters in lowercase (a to z )
+# Identifiers are case-sensitive. uppercase letters(A to Z)
+# It's a convention to start an identifier with a letter rather _.
+# Whitespaces are not allowed.
+# We cannot use special symbols like !, @, #, $, and so on.
+
+val2 = 10
+val_ = 99
+print(val2,val_)
+
